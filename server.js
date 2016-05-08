4ad949ca1167613debea7bbfbcb67e580fa55820
Steve Smock <SSmock@cubesmart.com>
2016-05-08 15:15:32 -0400

    Removed npm log; added web server setup

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 "use strict";
 
-const verbose = true;
 const httpPort = process.env.PORT || 4004;
 
 const UUID = require('node-uuid');
@@ -10,12 +9,31 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+startHttpServer();
+startSocketServer();
 
-app.use(express.static('lib'));
+function startHttpServer() {
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+  });
 
-http.listen(httpPort, function() {
-    console.log("listening on port " + httpPort);
-});
+  app.use(express.static('lib'));
+
+  http.listen(httpPort, function() {
+      console.log("listening on port " + httpPort);
+  });
+}
+
+function startSocketServer() {
+  io.on('connection', function (client) {
+    client.UserID = UUID();
+
+    client.emit('onconnected', { ID: client.UserID } );
+
+    // TODO: Log connection
+
+    //client.on('disconnect', function () {
+    //  TODO: Log disconnection
+    //});
+  });
+}
